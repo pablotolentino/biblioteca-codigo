4ba25cc05820ab7105f3d3c66b72731089176021
Pablo Tolentino <pablotolentino@outlook.com>
2018-06-01 22:22:18 -0500

    avance

---
+++ Blink.CodeGenerator/CodeGenerator.CodeConfiguration/Configuration.cs
@@ -0,0 +1,38 @@
+﻿using CodeGenerator.Interfaces;
+using System;
+
+namespace CodeGenerator.CodeConfiguration
+{
+    public class Configuration : ICodeConfiguration
+    {
+        private string _characteres;
+        public Configuration()
+        {
+
+        }
+        public void SetCharacters(string characteres)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetIsAutoincrement(bool isAutoincrement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLength(int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPattern(string pattern)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetStartingFrom(string startingFrom)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
